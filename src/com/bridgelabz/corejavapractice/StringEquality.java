32169c2b3489a5c36e4018675bf0fa56dc84bf50
CHANDANAcs02 <chandanacs2004@gmail.com>
2026-06-16 16:26:13 +0530

    [Chandana]: String Equality check

---
+++ src/com/bridgelabz/corejavapractice/StringEquality.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.corejavapractice;
+
+public class StringEquality {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+        String str1 = "Hello";
+        String str2 = "Hello";
+
+        if (str1.equals(str2)) 
+        {
+            System.out.println("Strings are equal");
+        } else 
+        {
+            System.out.println("Strings are not equal");
+        }
+
+	}
+
+}
