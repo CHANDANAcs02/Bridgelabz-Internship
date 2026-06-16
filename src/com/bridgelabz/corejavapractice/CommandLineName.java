c7a158b955af34d50959d07f327b718c0afc357f
CHANDANAcs02 <chandanacs2004@gmail.com>
2026-06-16 16:35:49 +0530

    [Chandana]: Command Line Name Program

--- src/com/bridgelabz/corejavapractice/CommandLineName.java
+++ src/com/bridgelabz/corejavapractice/CommandLineName.java
@@ -4,6 +4,15 @@ public class CommandLineName {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		    if (args.length > 0) 
+		    {
+	            System.out.println("Hello " + args[0]);
+	        }
+	        else 
+	        {
+	            System.out.println("No name entered");
+	        }
 
 	}
 
