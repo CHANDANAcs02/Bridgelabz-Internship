405907925d0230a24e42a0c9910deed6f6f9dd83
ChandanaCS2004 <chandanacs2004@gmail.com>
2026-06-16 15:23:01 +0530

    Chandana: Display Message Program

---
+++ src/com/bridgelabz/corejavapractice/DisplayMessage.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.corejavapractice;
+
+public class DisplayMessage 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Helloooo, Welocome to core Java Practice");
+	}
+
+}
