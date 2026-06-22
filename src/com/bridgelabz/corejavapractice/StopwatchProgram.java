9c5f55c66a8d942204620f3c924a69f8fc1e61bb
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-06-22 07:49:28 +0530

    [Chandana]: Simulate Stopwatch Program

---
+++ src/com/bridgelabz/corejavapractice/StopwatchProgram.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.corejavapractice;
+
+import java.util.Scanner;
+
+public class StopwatchProgram {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Press Enter to Start Stopwatch...");
+        sc.nextLine();
+
+        long startTime = System.currentTimeMillis();
+
+        System.out.println("Press Enter to Stop Stopwatch...");
+        sc.nextLine();
+
+        long endTime = System.currentTimeMillis();
+
+        long elapsedTime = endTime - startTime;
+
+        System.out.println("Elapsed Time = "
+                + elapsedTime + " milliseconds");
+
+        System.out.println("Elapsed Time = "
+                + (elapsedTime / 1000.0) + " seconds");
+    }
+}
