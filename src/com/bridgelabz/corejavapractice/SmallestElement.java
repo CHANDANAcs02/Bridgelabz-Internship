b840146c9f9ba6b88e60d7528be28d4f1406072a
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-06-22 06:53:13 +0530

    [Chandana]: Find Smallest Element in an Array

---
+++ src/com/bridgelabz/corejavapractice/SmallestElement.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.corejavapractice;
+
+public class SmallestElement {
+
+	public static void main(String[] args) {
+
+		int arr[] = {25,11,7,75,56};
+
+		int min = arr[0];
+
+		for(int i = 1; i < arr.length; i++) {
+
+			if(arr[i] < min)
+				min = arr[i];
+		}
+
+		System.out.println("Smallest Element = " + min);
+	}
+}
