06de55f9d9255b51cfc866af4261ca873d4e3f51
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-06-22 06:52:16 +0530

    [Chandana]: Find Largest Element in an Array

---
+++ src/com/bridgelabz/corejavapractice/LargestElement.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.corejavapractice;
+
+public class LargestElement {
+
+	public static void main(String[] args) {
+
+		int arr[] = {25,11,7,75,56};
+
+		int max = arr[0];
+
+		for(int i = 1; i < arr.length; i++) {
+
+			if(arr[i] > max)
+				max = arr[i];
+		}
+
+		System.out.println("Largest Element = " + max);
+	}
+}
