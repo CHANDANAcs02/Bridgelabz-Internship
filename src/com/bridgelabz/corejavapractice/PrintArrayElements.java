7beb1361198c45abae4aff0e78f2668a35dd4714
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-06-22 06:48:55 +0530

    [Chandana]: Print Elements of an Array

---
+++ src/com/bridgelabz/corejavapractice/PrintArrayElements.java
@@ -0,0 +1,14 @@
+
+package com.bridgelabz.corejavapractice;
+
+public class PrintArrayElements {
+
+	public static void main(String[] args) {
+
+		int arr[] = {10, 20, 30, 40, 50};
+
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
