dd754820a2719a92c4d4f097369466afda88b84e
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-06-24 14:21:00 +0530

    [Chandana] : UC1 Check Employee Attendance

--- src/com/bridgelabz/oops/EmployeeWageComputation.java
+++ src/com/bridgelabz/oops/EmployeeWageComputation.java
@@ -4,7 +4,16 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 
-		// Welcome message
 		System.out.println("Welcome to Employee Wage Computation Program");
+
+		// Generate random attendance
+		// 0 = Absent, 1 = Present
+		int isPresent = (int)(Math.random() * 2);
+
+		if (isPresent == 1) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
