f17e85f656b335be3d646c36fb89c0b8c6c1d5a8
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-06-24 12:27:18 +0530

    [Chandana] :Core Java practice programs

--- src/com/bridgelabz/corejavapractice/SumNaturalFor.java
+++ src/com/bridgelabz/corejavapractice/SumNaturalFor.java
@@ -1,5 +1,21 @@
 package com.bridgelabz.corejavapractice;
 
-public class SumNaturalFor {
+import java.util.Scanner;
 
+public class SumNaturalFor {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for(int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
 }
